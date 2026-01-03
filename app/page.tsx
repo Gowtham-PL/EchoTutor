9e66b422092f5223e156a384af6e037dffcfb9a0
Gowtham Palaniappan <gowthampalaniappan1881@gmail.com>
2026-01-04 00:49:56 +0530

    Implemented authetAuth,Billing,Database Setup,Create Comapanion server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>
-          Lets get started
-      </Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+          <CompanionCard
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="science"
+            duration={45}
+            color="#E5D0FF"
+
+          />
+          <CompanionCard
+              id="789"
+              name="Verba the Vocabulary Builder"
+              topic="English Literature"
+              subject="Language"
+              duration={30}
+              color="#BDE7FF"
+          />
+          <CompanionCard
+              id="123"
+              name="Neura the Brainy Explorer"
+              topic="Neural Network of the Brain"
+              subject="science"
+              duration={45}
+              color="#ffda6e"
+          />
+      </section>
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="2-2/3 max-lg:w-full"
+            />
+            <CTA />
+        </section>
+    </main>
   )
 }
 
