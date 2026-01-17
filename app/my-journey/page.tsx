0e3e40ce2e4292f1351b195a55c80b4bdfcda50a
Gowtham Palaniappan <gowthampalaniappan1881@gmail.com>
2026-01-17 10:59:47 +0530

    fixed bugs

--- app/my-journey/page.tsx
+++ app/my-journey/page.tsx
@@ -49,7 +49,7 @@ const Profile = async() => {
                             </p>
                         </div>
                         <div>
-                            Lessons completed
+                            Companions Created
                         </div>
                     </div>
                 </div>
